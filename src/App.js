40972891713f8971abeb97280abe76c066a39307
Aatif-007 <145668612+Aatif-007@users.noreply.github.com>
2024-07-31 17:35:35 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Wheather from './Components/Wheather';
+
+function App() {
+
+
+  return (
+    
+    <div>
+      <Wheather/>
+    </div>
+  );
+}
+
+export default App;
